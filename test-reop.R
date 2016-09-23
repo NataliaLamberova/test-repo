cb3434c1cbd3a0e730a068e660088daee1daf385
Natalia Lamberova <natalia.lamberova@gmail.com>
2016-09-22 17:28:47 -0700

    Hello World

--- test-reop.R
+++ test-reop.R
@@ -1,2 +1,10 @@
 
 print("Hello World!")
+install.packages("maps")
+#Note that the package got installed into your Rproject directory
+system.file(package="maps")
+library(maps)
+packrat::snapshot()
+#Takes a snapshot of all your libraries
+packrat::restore()
+#Restrores exact same packcges(and their versions on you collaborator's computer)
