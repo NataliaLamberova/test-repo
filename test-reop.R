d67190cdb9881373fed9ccad188bfe3900c807db
Natalia Lamberova <natalia.lamberova@gmail.com>
2016-09-26 08:40:54 -0700

    What is commit

--- test-reop.R
+++ test-reop.R
@@ -8,3 +8,10 @@ packrat::snapshot()
 #Takes a snapshot of all your libraries
 packrat::restore()
 #Restrores exact same packcges(and their versions on you collaborator's computer)
+
+##Commit consitsts of 5 parts:
+#id
+#author
+#changes
+#message
+#parent id
